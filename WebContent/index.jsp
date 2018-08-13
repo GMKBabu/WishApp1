f5b55e4a5b2c0d1e4da76179f0c0508cc7d2f8cf
babu <babu.g0730@gmail.com>
2018-08-13 15:42:01 +0530

    commited by babu

---
+++ WebContent/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Hi...GMK Babu</h1>
+<h2>HTML-SERVLET COMMUNICATION</h2>
+<a href="wurl">GenerateWishMessage</a>
+</body>
+</html>
---
+++ src/com/nt/servlet/WishServlet.java
@@ -0,0 +1,38 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WishServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+		Calendar calendar = Calendar.getInstance();
+		int hr = calendar.get(Calendar.HOUR_OF_DAY);
+		
+		
+	Date d = new Date();
+	pw.println("<b><i><center> Date and Time is :"+d+"</center></i></b>");
+	
+	if(hr <= 12)
+		pw.println("<h2>GOOD MORNING</h2>");
+	else if(hr <= 16)
+		pw.println("<h2>GOOD AFTERNOON</h2>");
+	else if(hr <= 20)
+		pw.println("<h2>GOOD EVENING</h2>");
+	else
+		pw.println("<h2>GOOD NIGHT</h2>");
+	}
+
+public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException {
+	doGet(req,res);
+}
+
+}
